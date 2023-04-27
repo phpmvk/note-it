e7586e86b0915ac02e47e2fe3dc322c7c6a050f6
Philip von Koss <phpmvk@gmail.com>
2023-04-27 15:48:09 +0200

    added an integration test, and some unit tests

---
+++ server/tests/mocks.js
@@ -0,0 +1,17 @@
+module.exports = {
+  testNote: {
+    title: 'Test title',
+    body: 'Test body',
+    date: new Date(),
+    notebook: 'Test notebook',
+    user: 1,
+    favorite: false,
+  },
+  badNote: {
+    title: 'bad note title',
+    body: 'bad note body',
+    notebook: 'bad notebook',
+    user: 1,
+    favorite: false,
+  }
+}
--- server/tests/router.test.js
+++ server/tests/router.test.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = require('../routes/router');
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
-const {getType} = require('jest-get-type');
+const { testNote, badNote }= require('./mocks');
+const { describe, expect } = require('@jest/globals');
 
 const app = express();
 app.use(express.json());
@@ -22,103 +23,90 @@ afterAll(async () => {
   await mongoServer.stop();
 });
 
-describe('GET /notes', () => {
-  it('should return a list of notes', async () => {
-    const res = await request(app).get('/notes');
-    expect(res.status).toBe(200);
+describe('UNIT TESTS', () => {
+
+  describe('GET /notes', () => {
+    it('should return a list of notes', async () => {
+      const res = await request(app).get('/notes');
+      expect(res.status).toBe(200);
+    });
+
+    it('should return an array', async () => {
+      const res = await request(app).get('/notes');
+      expect(Array.isArray(res.body)).toBe(true);
+    })
   });
 
-  it('should return an array', async () => {
-    const res = await request(app).get('/notes');
-    expect(Array.isArray(res.body)).toBe(true);
+  describe('POST /notes', () => {
+    it('should create a new note and return it', async () => {
+      const res = await request(app).post('/notes').send(testNote);
+      expect(res.status).toBe(201);
+      expect(res.body.title).toBe(testNote.title);
+    });
+
+    it('should return an error when there is no date property', async ()=> {
+      const res = await request(app).post('/notes').send(badNote);
+      expect(res.status).toBe(500)
+    })
+
+  });
+
+  // Add similar test cases for other routes, i.e., getNote, updateNote, and deleteNote
+  describe('GET /notes/:id', () => {
+    it('should return a note with a specific id', async () => {
+      const postRes = await request(app).post('/notes').send(testNote);
+      const noteId = postRes.body._id;
+
+      // Retrieve the created note
+      const getRes = await request(app).get(`/notes/${noteId}`);
+      expect(getRes.status).toBe(200);
+      expect(getRes.body.title).toBe(testNote.title);
+    });
+  });
+
+  describe('PUT /notes/:id', () => {
+    it('should update a note with a specific id and return the updated note', async () => {
+      const postRes = await request(app).post('/notes').send(testNote);
+      const noteId = postRes.body._id;
+      // Update the created note
+      const updatedNote = {
+        title: 'Updated title',
+        body: 'Updated body',
+        date: new Date(),
+        notebook: 'Updated notebook',
+        user: 2,
+        favorite: true,
+      };
+      const putRes = await request(app).put(`/notes/${noteId}`).send(updatedNote);
+      expect(putRes.status).toBe(200);
+      expect(putRes.body.title).toBe(updatedNote.title);
+      expect(putRes.body.body).toBe(updatedNote.body);
+    });
+  });
+
+  describe('DELETE /notes/:id', () => {
+    it('should delete a note with a specific id and return the deleted note', async () => {
+      const postRes = await request(app).post('/notes').send(testNote);
+      const noteId = postRes.body._id;
+
+      // Delete the created note
+      const deleteRes = await request(app).delete(`/notes/${noteId}`);
+      expect(deleteRes.status).toBe(200);
+      expect(deleteRes.body.title).toBe(testNote.title);
+    });
+  });
+
+})
+
+describe('INTEGRATION TESTS', () => {
+  
+  it('should retrieve posted note from DB', async () => {
+    await request(app).post('/notes').send(testNote);
+    const res = await request(app).get('/notes')
+    expect(res.body[0].title).toBe(testNote.title);
   })
-});
+  
+  
 
-describe('POST /notes', () => {
-  it('should create a new note and return it', async () => {
-    const newNote = {
-      title: 'Test title',
-      body: 'Test body',
-      date: new Date(),
-      notebook: 'Test notebook',
-      user: 1,
-      favorite: false,
-    };
-    const res = await request(app).post('/notes').send(newNote);
-    expect(res.status).toBe(201);
-    expect(res.body.title).toBe(newNote.title);
-  });
-});
 
-// Add similar test cases for other routes, i.e., getNote, updateNote, and deleteNote
-describe('GET /notes/:id', () => {
-  it('should return a note with a specific id', async () => {
-    // Create a new note
-    const newNote = {
-      title: 'Test title',
-      body: 'Test body',
-      date: new Date(),
-      notebook: 'Test notebook',
-      user: 1,
-      favorite: false,
-    };
-    const postRes = await request(app).post('/notes').send(newNote);
-    const noteId = postRes.body._id;
-
-    // Retrieve the created note
-    const getRes = await request(app).get(`/notes/${noteId}`);
-    expect(getRes.status).toBe(200);
-    expect(getRes.body.title).toBe(newNote.title);
-  });
-});
-
-describe('PUT /notes/:id', () => {
-  it('should update a note with a specific id and return the updated note', async () => {
-    // Create a new note
-    const newNote = {
-      title: 'Test title',
-      body: 'Test body',
-      date: new Date(),
-      notebook: 'Test notebook',
-      user: 1,
-      favorite: false,
-    };
-    const postRes = await request(app).post('/notes').send(newNote);
-    const noteId = postRes.body._id;
-
-    // Update the created note
-    const updatedNote = {
-      title: 'Updated title',
-      body: 'Updated body',
-      date: new Date(),
-      notebook: 'Updated notebook',
-      user: 2,
-      favorite: true,
-    };
-    const putRes = await request(app).put(`/notes/${noteId}`).send(updatedNote);
-    expect(putRes.status).toBe(200);
-    expect(putRes.body.title).toBe(updatedNote.title);
-    expect(putRes.body.body).toBe(updatedNote.body);
-  });
-});
-
-describe('DELETE /notes/:id', () => {
-  it('should delete a note with a specific id and return the deleted note', async () => {
-    // Create a new note
-    const newNote = {
-      title: 'Test title',
-      body: 'Test body',
-      date: new Date(),
-      notebook: 'Test notebook',
-      user: 1,
-      favorite: false,
-    };
-    const postRes = await request(app).post('/notes').send(newNote);
-    const noteId = postRes.body._id;
-
-    // Delete the created note
-    const deleteRes = await request(app).delete(`/notes/${noteId}`);
-    expect(deleteRes.status).toBe(200);
-    expect(deleteRes.body.title).toBe(newNote.title);
-  });
-});
+})
