34dfb373e440798e96bfb75d896f2e98f1af0b08
Idar <idarnigatu@gmail.com>
2023-04-28 13:31:55 +0200

    fix: cleaned up login tests

--- client/note_it/src/__test__/Login.test.js
+++ client/note_it/src/__test__/Login.test.js
@@ -1,41 +1,39 @@
+/* eslint-disable testing-library/no-unnecessary-act */
 import { render, screen } from '@testing-library/react';
-import { Login } from '../pages/Login';
-import { MemoryRouter, BrowserRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
 import App from '../App';
 
-describe('Login Page', () => { 
+describe('Login Page', () => {
   it('renders username & password input', async () => {
-    render(<App/>)
-    const userName = await screen.getByTestId('userNameInput');
-    const userPassword = await screen.getByTestId('userPassword')
+    render(<App />);
+    const userName = screen.getByTestId('userNameInput');
+    const userPassword = screen.getByTestId('userPassword');
     expect(userName).toBeInTheDocument();
     expect(userPassword).toBeInTheDocument();
   });
-  
+
   it('should redirect users to /home upon login', async () => {
     window.history.pushState({}, 'Login page', '/login');
-    render(<App/>)
-    const button = screen.getByTestId('loginButton')
-    expect(button).toBeInTheDocument() 
+    render(<App />);
+    const button = screen.getByTestId('loginButton');
+    expect(button).toBeInTheDocument();
     await act(async () => {
       userEvent.click(button);
-    })
+    });
     expect(screen.getByTestId('homeDiv')).toBeInTheDocument();
     expect(window.location.pathname).toBe('/home');
   });
 
   it('should redirect users to /signup upon Register now!', async () => {
     window.history.pushState({}, 'Login page', '/login');
-    render(<App />)
-    const button = screen.getByTestId('registerButton')
-    expect(button).toBeInTheDocument() 
+    render(<App />);
+    const button = screen.getByTestId('registerButton');
+    expect(button).toBeInTheDocument();
     await act(async () => {
       userEvent.click(button);
-    })
+    });
     expect(screen.getByTestId('signupDiv')).toBeInTheDocument();
     expect(window.location.pathname).toBe('/signup');
   });
-
- })
+});
